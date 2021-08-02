30da248ca6f6597e6c133bb3cedb90215cb9af2a
Mhd Ramadhan Arvin <mramadhan687@gmail.com>
2021-08-03 01:52:32 +0700

    add command to project

--- config/telegram.php
+++ config/telegram.php
@@ -39,14 +39,10 @@ return [
             'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
             'commands'            => [
                 // Telegram\Bot\Commands\HelpCommand::class,
-                \App\Telegram\Commands\StartCommand::class,
-                // \App\Telegram\Commands\MyAlarmCommand::class,
-                // \App\Telegram\Commands\SetAlarmCommand::class,
-                // \App\Telegram\Commands\UpdateAlarmCommand::class,
-                // \App\Telegram\Commands\DeleteAlarmCommand::class,
-                // \App\Telegram\Commands\AbsensiCommand::class,
-                // \App\Telegram\Commands\ReportCommand::class,
                 \App\Telegram\Commands\AuthCommand::class,
+                \App\Telegram\Commands\StartCommand::class,
+                \App\Telegram\Commands\StatusCommand::class,
+                \App\Telegram\Commands\NotificationCommand::class,
             ],
         ],
 
